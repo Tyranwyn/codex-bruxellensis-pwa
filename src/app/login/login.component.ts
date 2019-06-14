ccb38211eea596a69ad6b55dc1446fb1120a5e50
Sammi Fux <sammi.fux@gmail.com>
2019-06-15 01:37:26 +0200

    login logic in services

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { auth } from 'firebase';
 import {AuthProviders} from '../models/auth-providers.enum';
+import {AuthenticationService} from '../services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -17,7 +17,8 @@ export class LoginComponent implements OnInit {
   password;
 
   constructor(private router: Router,
-              private angularFireAuth: AngularFireAuth) {
+              private angularFireAuth: AngularFireAuth,
+              private authService: AuthenticationService) {
     this.angularFireAuth.user.subscribe(user => {
       if (user) {
         router.navigate(['']);
@@ -29,25 +30,12 @@ export class LoginComponent implements OnInit {
   }
 
   login(provider: string) {
-    switch (provider) {
-      case AuthProviders.Google:
-        console.log(provider);
-        this.angularFireAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
-        break;
-      case AuthProviders.Facebook:
-        this.angularFireAuth.auth.signInWithPopup(new auth.FacebookAuthProvider());
-        break;
-      case AuthProviders.Twitter:
-        this.angularFireAuth.auth.signInWithPopup(new auth.TwitterAuthProvider());
-        break;
-      case AuthProviders.GitHub:
-        this.angularFireAuth.auth.signInWithPopup(new auth.GithubAuthProvider());
-        break;
-      case 'Email':
-        this.angularFireAuth.auth.createUserWithEmailAndPassword(this.email, this.password);
-        break;
-      default:
-        console.log(`The provider: ${provider}, is not configured yet.`);
+    if (provider === 'Email') {
+      this.authService.login(provider, this.email, this.password)
+        .catch(reason => console.log(reason));
+    } else {
+      this.authService.login(provider)
+        .catch(reason => console.log(reason));
     }
   }
 }
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import {AuthProviders} from '../models/auth-providers.enum';
+import {auth} from 'firebase';
+import {AngularFireAuth} from '@angular/fire/auth';
+import UserCredential = firebase.auth.UserCredential;
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  constructor(private angularFireAuth: AngularFireAuth) { }
+
+  login(provider: string, email?: string, password?: string): Promise<UserCredential> {
+    switch (provider) {
+      case AuthProviders.Google:
+        return this.angularFireAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+      case AuthProviders.Facebook:
+        return this.angularFireAuth.auth.signInWithPopup(new auth.FacebookAuthProvider());
+      case AuthProviders.Twitter:
+        return this.angularFireAuth.auth.signInWithPopup(new auth.TwitterAuthProvider());
+      case AuthProviders.GitHub:
+        return this.angularFireAuth.auth.signInWithPopup(new auth.GithubAuthProvider());
+      case 'Email':
+        return this.angularFireAuth.auth.createUserWithEmailAndPassword(email, password);
+      default:
+        console.log(`The provider: ${provider}, is not configured yet.`);
+    }
+  }
 }
