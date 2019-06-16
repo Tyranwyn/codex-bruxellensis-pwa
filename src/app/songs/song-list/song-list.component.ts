b6c2734bc58ef6ff5ae5dc7ce6e4734ced41e9de
Sammi Fux <sammi.fux@gmail.com>
2019-06-16 19:00:00 +0200

    page sorting

--- src/app/songs/song-list/song-list.component.ts
+++ src/app/songs/song-list/song-list.component.ts
@@ -1,7 +1,18 @@
-import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
-import {Song} from '../../models/song';
-import {SongService} from '../../services/song-service';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Song } from '../../models/song';
+import { SongService } from '../../services/song-service';
+import { map } from 'rxjs/operators';
+
+const comparePageNumbers = (a: Song, b: Song) => {
+  if (a.page < b.page) {
+    return -1;
+  }
+  if (a.page > b.page) {
+    return 1;
+  }
+  return 0;
+};
 
 @Component({
   selector: 'app-song-list',
@@ -13,7 +24,10 @@ export class SongListComponent implements OnInit {
   $songs: Observable<Song[]>;
 
   constructor(private songService: SongService) {
-    this.$songs = songService.getAllSongs();
+    this.$songs = songService.getAllSongs()
+      .pipe(
+        map(s => s.sort(comparePageNumbers))
+      );
   }
 
   ngOnInit() {
