f22ff1e6fabeb14cf58ec09048d5f632c1eb6568
Sammi Fux <sammi.fux@gmail.com>
2020-04-26 17:29:11 +0200

    update deployment files

--- deploy.js
+++ deploy.js
@@ -1,3 +1,35 @@
-let AWS = require("aws-sdk");
+const fs = require("fs");
+const {S3} = require("aws-sdk");
 
-var s3 = new AWS.S3({apiVersion: '2006-03-01'});
+let s3 = new S3({
+  apiVersion: "2006-03-01",
+  region: "eu-west-3"
+});
+
+const bucket = process.env.S3_BUCKET;
+var params = {
+  Bucket: bucket, /* required */
+};
+s3.listObjectsV2(params, (err, data) => {
+  if (err) console.log(err, err.stack); // an error occurred
+  else {
+    data.Contents.forEach(value => {
+      s3.deleteObject({Key: value.Key, Bucket: bucket}, deleteErr => {
+        if (deleteErr) console.log(deleteErr, deleteErr.stack);
+        else console.log(value.Key + " succesfully removed");
+      });
+    });
+    fs.readdirSync("dist").forEach(itemName => {
+      fs.readFile("dist/" + itemName, (readErr, file) => {
+        s3.putObject({
+          Bucket: bucket,
+          Key: itemName,
+          Body: JSON.stringify(file, null, 2)
+        }, (s3Err, data) => {
+          if (s3Err) throw s3Err
+          console.log(`File uploaded successfully at ${data.Location}`);
+        });
+      });
+    });
+  }
+});
